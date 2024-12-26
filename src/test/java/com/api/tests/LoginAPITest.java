345cda39a4d30b461f2db81ab895ed2633795031
Rukshan Dissanayake <rukshanmali@gmail.com>
2024-12-27 00:19:50 +0200

    Login API scripting

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,22 @@
+package com.api.tests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class LoginAPITest {
+
+	@Test(description = "Check if login API works!")
+	public void loginTest() {
+		RestAssured.baseURI = "http://64.227.160.186:8080";
+		RequestSpecification x = RestAssured.given();
+		RequestSpecification y = x.header("Content-Type", "application/json");
+		RequestSpecification z = y
+				.body("{\r\n" + "  \"username\": \"uday1234\",\r\n" + "  \"password\": \"uday1234\"\r\n" + "}");
+		Response response = z.post("/api/auth/login");
+		System.out.println(response.asPrettyString());
+	}
+
+}
