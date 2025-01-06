0abd298f66788d0ccb955978be8924e657e535c2
Rukshan Dissanayake <rukshanmali@gmail.com>
2025-01-07 01:08:43 +0200

    applying a design pattern to reduce code lines

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -12,15 +12,13 @@ public class LoginAPITest {
 	@Test(description = "Check if login API works!")
 	public void loginTest() {
 		RestAssured.baseURI = "http://64.227.160.186:8080";
-		RequestSpecification x = RestAssured.given();
-		RequestSpecification y = x.header("Content-Type", "application/json");
+		RequestSpecification y = RestAssured.given().header("Content-Type", "application/json");
 		RequestSpecification z = y
 				.body("{\r\n" + "  \"username\": \"uday1234\",\r\n" + "  \"password\": \"uday1234\"\r\n" + "}");
 		Response response = z.post("/api/auth/login");
 		System.out.println(response.asPrettyString());
 
 		Assert.assertEquals(response.getStatusCode(), 200);
-		//this needs to update with coding best practices
 	}
 
 }
