082028f0f8a011e6815014500ba7988827f0a1ee
Rukshan Dissanayake <rukshanmali@gmail.com>
2025-01-07 01:18:14 +0200

    Improved Readability using Static imports and BaseUri

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -3,17 +3,14 @@ package com.api.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 public class LoginAPITest {
 
 	@Test(description = "Check if login API works!")
 	public void loginTest() {
-		RestAssured.baseURI = "http://64.227.160.186:8080";
-
-		Response response = RestAssured.given().header("Content-Type", "application/json")
+		Response response = given().baseUri("http://64.227.160.186:8080").header("Content-Type", "application/json")
 				.body("{\r\n" + "  \"username\": \"uday1234\",\r\n" + "  \"password\": \"uday1234\"\r\n" + "}")
 				.post("/api/auth/login");
 		System.out.println(response.asPrettyString());
