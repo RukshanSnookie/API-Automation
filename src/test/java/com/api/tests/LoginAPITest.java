c85a53a997398fd70970a54cf258057758e52029
Rukshan Dissanayake <rukshanmali@gmail.com>
2025-01-15 23:52:41 +0200

    Remove unwanted code lines for better code quality

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -13,9 +13,6 @@ public class LoginAPITest {
 
 	@Test(description = "Check if login API works!")
 	public void loginTest() {
-//		Response response = given().baseUri("http://64.227.160.186:8080").header("Content-Type", "application/json")
-//				.body("{\r\n" + "  \"username\": \"uday1234\",\r\n" + "  \"password\": \"uday1234\"\r\n" + "}")
-//				.post("/api/auth/login");
 
 		LoginRequest loginRequest = new LoginRequest("uday1234", "uday1234");
 		AuthService authService = new AuthService();
@@ -25,7 +22,7 @@ public class LoginAPITest {
 
 		LoginResponse loginResponse = response.as(LoginResponse.class);
 		System.out.println("Id = " + loginResponse.getId());
-		
+
 		Assert.assertTrue(loginResponse.getToken() != null);
 		Assert.assertEquals(loginResponse.getId(), 3);
 	}
